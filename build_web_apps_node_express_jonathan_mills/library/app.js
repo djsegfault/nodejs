9b2b4517f3704c0e387bedb0ce1d23e1f8a40031
djsegfault <david@thekramers.net>
2020-04-18 14:09:42 -0400

    Added morgan for logging, path for platform-independent path building, serving static html

--- build_web_apps_node_express_jonathan_mills/library/app.js
+++ build_web_apps_node_express_jonathan_mills/library/app.js
@@ -1,15 +1,31 @@
 // Pull in the dependencies
 var express = require('express');
 var chalk = require('chalk');
+var debug = require('debug')('app');
+var morgan = require('morgan');
+var path = require('path');
 
 // Globals
 var app = express();
 
+// Set up Morgan for access logging
+app.use(morgan('combined'));
+
 // Route for / to anonymous handler function 
 app.get('/', function (req, res) {
-	res.send("Hello from my library app");
+	// res.send("Hello from my library app");
+	res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 	
 app.listen(3000, function () {
-	console.log('Listening on port ' + chalk.green('3000') + '.');
+	// Old school way
+	// console.log('Listening on port ' + chalk.green('3000'););
+
+	// With template strings, surrounded by backticks
+	//console.log(`Listening on port ${chalk.green('3000')}.`);
+
+	// debug mode, only shows up when you're debugging
+	// To run with debugging:
+	// DEBUG=* node app.js
+	debug(`Listening on port ${chalk.green('3000')}.`);
 });
