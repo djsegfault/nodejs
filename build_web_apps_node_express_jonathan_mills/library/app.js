cbb324e6b2854c3c53d456384d96015422fe56ba
djsegfault <david@thekramers.net>
2020-04-19 14:33:01 -0400

    Did manual fixing as per lint, before lint fix

--- build_web_apps_node_express_jonathan_mills/library/app.js
+++ build_web_apps_node_express_jonathan_mills/library/app.js
@@ -1,12 +1,12 @@
 // Pull in the dependencies
-var express = require('express');
-var chalk = require('chalk');
-var debug = require('debug')('app');
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 // Globals
-var app = express();
+const app = express();
 
 // Set up Morgan for access logging
 app.use(morgan('combined'));
@@ -16,24 +16,28 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Set up Express to find Bootstrap and Jquery from the node modules
 app.use('/css', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'css')));
-app.use('/js',  express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
-app.use('/js',  express.static(path.join(__dirname, 'node_modules', 'jquery',    'dist')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'bootstrap', 'dist', 'js')));
+app.use('/js', express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist')));
 
 // Route for / to index.html
+/* app.get('/', function (req, res) {
+  // res.send("Hello from my library app");
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+}); */
 app.get('/', function (req, res) {
-	// res.send("Hello from my library app");
-	res.sendFile(path.join(__dirname, 'views', 'index.html'));
+  // res.send("Hello from my library app");
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
-	
+
 app.listen(3000, function () {
-	// Old school way
-	// console.log('Listening on port ' + chalk.green('3000'););
+  // Old school way
+  // console.log('Listening on port ' + chalk.green('3000'););
 
-	// With template strings, surrounded by backticks
-	//console.log(`Listening on port ${chalk.green('3000')}.`);
+  // With template strings, surrounded by backticks
+  //console.log(`Listening on port ${chalk.green('3000')}.`);
 
-	// debug mode, only shows up when you're debugging
-	// To run with debugging:
-	// DEBUG=* node app.js
-	debug(`Listening on port ${chalk.green('3000')}.`);
+  // debug mode, only shows up when you're debugging
+  // To run with debugging:
+  // DEBUG=* node app.js
+  debug(`Listening on port ${chalk.green('3000')}.`);
 });
